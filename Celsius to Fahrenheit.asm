63607af52e77535e6ae6816162ec8a8ccaf9c843
Mohammad Mohitul Islam <mohitul838@gmail.com>
2021-03-08 12:38:35 +0600

    Update and rename README.md to Celsius to Fahrenheit.asm

---
+++ Celsius to Fahrenheit.asm
@@ -0,0 +1,50 @@
+# Microprocessor-Lab
+        ;INTPUT TWO DIGIT
+MOV AH,1H   
+INT 21H
+
+SUB AL,'0'  
+MOV CL,AL
+     
+MOV AH,01H  
+INT 21H
+
+SUB AL,'0'  
+
+XCHG AL,CL
+          
+MOV AH,10    
+MUL AH
+ADD AL,CL
+
+         ; C TO F
+
+MOV BH,9
+MUL BH
+
+MOV BH,5
+DIV BH
+
+ADD AL,32
+
+;PRINT TWO DIGIT
+MOV AH,0    ;Initail 0 as remainder
+
+MOV BH,10
+DIV BH
+
+MOV BL,AL
+MOV CL,AH   ;REMAINDER
+
+MOV DL,BL
+ADD DL,30H
+MOV AH,2   
+INT 21H
+
+MOV DL,CL
+ADD DL,30H
+MOV AH,2   
+INT 21H
+
+
+
